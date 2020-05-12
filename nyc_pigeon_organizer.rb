c8983742ebc6d5f7d395563207e6bb6cc4281584
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-12 01:10:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each { |key, value|
+    value.each {|vals, names|
+      names.each {|name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << vals.to_s
+      }
+    }
+  }
+  new_hash
 end
