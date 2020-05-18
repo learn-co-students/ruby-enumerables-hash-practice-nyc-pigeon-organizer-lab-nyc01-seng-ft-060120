01910f4e0b85709152ed9cb7bd902111d9af7488
Adam Altmark <adamaltmark@gmail.com>
2020-05-18 14:54:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_hash = {}
+  data.each do |key, value|
+  value.each do |new_value, names|
+  names.each do |new_name|
+    if !final_hash[new_name]
+      final_hash[new_name] = {}
+    end
+    if !final_hash[new_name][key]
+      final_hash[new_name][key] = []
+    end
+    string_value = new_value.to_s
+    final_hash[new_name][key] << string_value
+  end
+  end
+  end
+  return final_hash
 end
