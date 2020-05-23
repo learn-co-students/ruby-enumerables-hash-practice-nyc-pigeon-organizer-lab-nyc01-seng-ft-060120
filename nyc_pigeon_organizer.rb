9fc895cc4024a5f379e1738210eba4e90481e229
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-23 17:52:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
 final_results = data.each_with_object({}) do |(key, value), array|
   value.each do |inner_key, names|
@@ -9,6 +10,7 @@ final_results = data.each_with_object({}) do |(key, value), array|
         !array[name][key] = []
       end 
       array[name][key].push(inner_key.to_s)
+      binding.pry 
     end 
 end
 end 
